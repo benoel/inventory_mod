20d6ac5dd8ce7ac760736255b9e2a666994b8af2
Ibnu Abdul Azis <ibnuazis@IBNU-MBP.lan>
2017-03-03 18:55:18 +0700

    add migration table and home page

---
+++ database/migrations/2017_03_03_072722_create_purchases_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePurchasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('supplier_id');
+            $table->integer('total_price');
+            $table->string('type');
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchases');
+    }
+}
---
+++ database/migrations/2017_03_03_072818_create_sales_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('customer_id');
+            $table->integer('total_price');
+            $table->string('type');
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
